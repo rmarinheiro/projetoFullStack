d2b95d94dd86f8051d4384d195d1c89ce3514422
Rafael <rsmarinheiro--7@gmail.com>
2022-07-13 18:08:58 -0300

    Project Created

---
+++ rafametas/src/main/java/com/rafael/rafametas/RafametasApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.rafametas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafametasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafametasApplication.class, args);
+	}
+
+}
