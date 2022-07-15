7268c8217d03e37bc12edbba4a2a93be0e4efd4e
Rafael <rsmarinheiro--7@gmail.com>
2022-07-15 00:18:59 -0300

    Consulta por Datas

--- rafametas/src/main/java/com/rafael/rafametas/controller/SaleController.java
+++ rafametas/src/main/java/com/rafael/rafametas/controller/SaleController.java
@@ -3,8 +3,11 @@ package com.rafael.rafametas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafael.rafametas.entity.Sale;
@@ -18,8 +21,10 @@ public class SaleController {
 	private SaleService saleService;
 	
 	@GetMapping
-	public List<Sale> findSales(){
-		return saleService.findSales();
+	public Page<Sale> findSales(@RequestParam(value = "minDate",defaultValue = "") String minDate, 
+								@RequestParam(value = "maxDate",defaultValue = "") String maxDate, Pageable pageable){
+		
+		return saleService.findSales(pageable,minDate,maxDate);
 	}
 
 }
--- rafametas/src/main/java/com/rafael/rafametas/service/SaleService.java
+++ rafametas/src/main/java/com/rafael/rafametas/service/SaleService.java
@@ -1,8 +1,12 @@
 package com.rafael.rafametas.service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.rafael.rafametas.entity.Sale;
@@ -14,9 +18,14 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
-	public List<Sale> findSales() {
+	public Page<Sale> findSales(Pageable pageable,String minDate,String maxDate) {
 		
-		return saleRepository.findAll();
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min =  minDate.equals("") ? today.minusDays(365): LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today: LocalDate.parse(maxDate);
+		
+		return saleRepository.findSales(min,max,pageable);
 		
 	}
 
