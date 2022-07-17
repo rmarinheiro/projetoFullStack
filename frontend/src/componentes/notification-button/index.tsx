8b8e7dbb03ea02fb84e288dacc79726fac210e53
Rafael <rsmarinheiro--7@gmail.com>
2022-07-17 01:05:23 -0300

    Toast retirado para testes

--- frontend/src/componentes/notification-button/index.tsx
+++ frontend/src/componentes/notification-button/index.tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 import './styles.css'
@@ -11,7 +10,7 @@ type Props = {
 function handleClick(id: number){
     axios(`${BASE_URL}/sales/${id}/notifications`)
         .then(response =>{
-            toast.info("SMS Enviado com Sucesso")
+            console.log("SMS ENviado")
         },)
 
 }
