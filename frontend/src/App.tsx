f1d1a51c6667d6374508caba13551f20ab6a0eb0
Rafael <rsmarinheiro--7@gmail.com>
2022-07-17 00:19:11 -0300

    Toasty

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import Header from "./componentes/Header"
 import NotificationButton from "./componentes/notification-button"
 import SalesCard from "./componentes/SalesCard"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <ToastContainer /> 
       <Header />
       <main>
         <section id="sales">
           <div className="rafametas-container">
-            <SalesCard/>
-          </div>  
+            <SalesCard />
+          </div>
         </section>
       </main>
     </>
